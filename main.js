2ac105d029f5cf70881c4abdf0d8ef66cc66b67d
Jauhen Feldman <feldmeow@gmail.com>
2020-08-25 16:05:59 +0300

    fix: get hiddenNum by random

--- main.js
+++ main.js
@@ -2,32 +2,35 @@
 let isNumber = function (n) {
   return !isNaN(parseFloat(n))
 };
-function hiddenNumber(hiddenNum, number) {
-  hiddenNum = 66;
-  number = (prompt("Угадай число от 1 до 100"));
+function hiddenNumber() {
+  const hiddenNum = Math.floor(Math.random() * 100);
+  function getNum() {
+    let number = (prompt("Угадай число от 1 до 100"));
 
-  function result() {
-    function alertMessage() {
-      if (+number === hiddenNum) {
-        return alert("Вы угадали!")
-      } else if (!isNumber(number)) {
-        return alert("Введи число!");
-      } else if (number > hiddenNum) {
-        return alert("Загаданное число меньше");
-      } else if (number < hiddenNum) {
-        return alert("Загаданное число больше");
+    function result() {
+      function alertMessage() {
+        if (+number === hiddenNum) {
+          return alert("Вы угадали!")
+        } else if (!isNumber(number)) {
+          return alert("Введи число!");
+        } else if (number > hiddenNum) {
+          return alert("Загаданное число меньше");
+        } else if (number < hiddenNum) {
+          return alert("Загаданное число больше");
+        };
+
       };
-
+      if (+number === hiddenNum) {
+        return alertMessage();
+      } else if (number === null) {
+        return false;
+      } else {
+        alertMessage();
+        getNum();
+      }
     };
-    if (+number === hiddenNum) {
-      return alertMessage();
-    } else if (number === null) {
-      return false;
-    } else {
-      alertMessage();
-      hiddenNumber();
-    }
-  };
-  result();
+    result();
+  }
+  getNum();
 }
 hiddenNumber();
